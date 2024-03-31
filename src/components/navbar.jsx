ead08d46d98912b9492c78c62df22623d49f2c89
nicktvdd <nicktvdd@gmail.com>
2024-03-31 20:34:05 +0300

    Image appearing over menu fixed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,10 @@
-"use client";
-import { useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import NavLink from "./navLink";
+"use client"
+
+import { useState } from "react"
+import Link from "next/link"
+import Image from "next/image"
+import NavLink from "./navLink"
+import { motion } from "framer-motion"
 
 const links = [
     { name: "Home", href: "/" },
@@ -14,11 +16,48 @@ const links = [
 const Navbar = () => {
     const [open, setOpen] = useState(false);
 
+    const topVariants = {
+        open: {
+            rotate: 45,
+            backgroundColor: "rgb(255,255,255)",
+        },
+        closed: {
+            rotate: 0,
+        },
+    }
+    const centerVariants = {
+        open: {
+            opacity: 0,
+        },
+        closed: {
+            opacity: 1,
+        },
+    }
+
+    const listVariants = {
+        open: {
+            x:0,
+        },
+        closed: {
+            x:"100vw",
+        },
+    }
+
+    const bottomVariants = {
+        open: {
+            rotate: -45,
+            backgroundColor: "rgb(255,255,255)",
+        },
+        closed: {
+            rotate: 0,
+        },
+    }
+
     return (
         <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px48 text-xl">
             {/* LINKS */}
             <div className="hidden md:flex gap-4 w-1/3">
-                {links.map(link=>(
+                {links.map(link => (
                     <NavLink link={link} key={link.name} />
                 ))}
             </div>
@@ -31,33 +70,47 @@ const Navbar = () => {
             </div>
             {/* SOCIALS */}
             <div className="hidden md:flex gap-4 w-1/3">
-                    <Link href="https://www.linkedin.com/in/nick-van-den-dungen/">
+                <Link href="https://www.linkedin.com/in/nick-van-den-dungen/">
                     <Image src="/linkedin.png" alt="" width={50} height={50} />
-                    </Link>
-                    <Link href="https://github.com/Nicktvdd">
+                </Link>
+                <Link href="https://github.com/Nicktvdd">
                     <Image src="/github.png" alt="" width={50} height={50} />
-                    </Link>
-                    <Link href="mailto:nicktvdd@gmail.com">
+                </Link>
+                <Link href="mailto:nicktvdd@gmail.com">
                     <Image src="/mail.png" alt="" width={50} height={50} />
-                    </Link>
+                </Link>
             </div>
             {/* RESPONSIVE MENU */}
             <div className="md:hidden">
                 {/* MENU BUTTON*/}
-                <button className="w-10 h-8 flex flex-col justify-between z-50 relative"  onClick={(()=>setOpen(!open))}>
-                    <div className="w-10 h-1 bg-white rounded"> </div>
-                    <div className="w-10 h-1 bg-white rounded"> </div>
-                    <div className="w-10 h-1 bg-white rounded"> </div>
+                <button className="w-10 h-8 flex flex-col justify-between z-50 relative"
+                onClick={() => setOpen((prev) => !prev)}
+                >
+                    <motion.div
+                        variants={topVariants}
+                        animate={open ? "open" : "closed"}
+                        className="w-10 h-1 bg-black rounded origin-left">
+                    </motion.div>
+                    <motion.div
+                        variants={centerVariants}
+                        animate={open ? "open" : "closed"}
+                        className="w-10 h-1 bg-black rounded">
+                    </motion.div>
+                    <motion.div
+                        variants={bottomVariants}
+                        animate={open ? "open" : "closed"}
+                        className="w-10 h-1 bg-black rounded origin-left">
+                    </motion.div>
                 </button>
                 {/* MENU LIST*/}
                 {open && (
-                <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
-                    {links.map((link) => (
-                        <Link href={link.href} key={link.name}>
-                            {link.name}
-                        </Link>
-                    ))}
-                </div>
+                    <motion.div variants={listVariants} initial="closed" animate="open" className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40">
+                        {links.map((link) => (
+                            <Link href={link.href} key={link.name}>
+                                {link.name}
+                            </Link>
+                        ))}
+                    </motion.div>
                 )}
             </div>
         </div>
