6079ab9180afade36efcc5670d44d404507f215b
nicktvdd <nicktvdd@gmail.com>
2024-04-02 12:25:53 +0300

    Frontpage links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image"
 import { motion } from "framer-motion"
+import Link from "next/link"
 
 const Homepage = () => {
   return (
@@ -25,8 +26,16 @@ const Homepage = () => {
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
-            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">View my work</button>
-            <button className="p-4 rounded-lg ring-1 ring-black">Contact me</button>
+            <Link href="/projects">
+              <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+                View my work
+              </button>
+            </Link>
+            <Link href="/contact">
+              <button className="p-4 rounded-lg ring-1 ring-black">
+                Contact me
+              </button>
+            </Link>
           </div>
         </div>
       </div>
