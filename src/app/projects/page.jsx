64bc30ff18d8f37d3f666f2a982dc34d92abdebe
nicktvdd <nicktvdd@gmail.com>
2024-04-04 12:01:31 +0300

    Scroll animation

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -25,7 +25,7 @@ const items = [
     id: 3,
     color: "from-pink-300 to-emerald-300",
     title: "The Mocks Website",
-    desc: "The Mocks is a Rock & Roll band from the Netherlands. I created a website for them using Wordpress, PHP and Elementor. The website is designed to be visually appealing, and has a Rock & Roll feeling. It includes a music player, custom tour plug-in, and a contact form.",
+    desc: "The Mocks is a Rock & Roll band from the Netherlands. I created a website for them using Wordpress, PHP and Elementor. The website is designed to be visually appealing, and has a Rock & Roll feeling. It includes a music player, custom tour plug-in, and lots of media",
     img: "/themocks.png",
     link: "https://themocksofficial.com/",
   },
@@ -54,7 +54,29 @@ const PortfolioPage = () => {
     >
       <div className="h-[600vh] relative" ref={ref}>
         <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
-          My Works
+          Check out my projects 
+          <motion.svg
+          initial={{ opacity: 0.2, y: 0 }}
+          animate={{ opacity: 1, y: "10px" }}
+          transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          width={50}
+          height={50}
+        >
+          <path
+            d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+            stroke="#000000"
+            strokeWidth="1"
+          ></path>
+          <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+          <path
+            d="M15 11L12 14L9 11"
+            stroke="#000000"
+            strokeWidth="1"
+          ></path>
+        </motion.svg>
         </div>
         <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
           <motion.div style={{ x }} className="flex">
@@ -84,7 +106,7 @@ const PortfolioPage = () => {
         </div>
       </div>
       <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
-        <h1 className="text-8xl">Do you have a project?</h1>
+        <h1 className="text-8xl">Let's collaborate!</h1>
         <div className="relative">
           <motion.svg
             animate={{ rotate: 360 }}
