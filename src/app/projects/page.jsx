411fbe93026567e79b301805dc6910d5f9897be4
nicktvdd <nicktvdd@gmail.com>
2024-04-04 15:36:56 +0300

    revert, bottom scroll not fixed yet

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -54,29 +54,29 @@ const PortfolioPage = () => {
     >
       <div className="h-[600vh] relative" ref={ref}>
         <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
-          Check out my projects
+          Check out my projects 
           <motion.svg
-            initial={{ opacity: 0.2, y: 0 }}
-            animate={{ opacity: 1, y: "10px" }}
-            transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            width={50}
-            height={50}
-          >
-            <path
-              d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
-              stroke="#000000"
-              strokeWidth="1"
-            ></path>
-            <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
-            <path
-              d="M15 11L12 14L9 11"
-              stroke="#000000"
-              strokeWidth="1"
-            ></path>
-          </motion.svg>
+          initial={{ opacity: 0.2, y: 0 }}
+          animate={{ opacity: 1, y: "10px" }}
+          transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          width={50}
+          height={50}
+        >
+          <path
+            d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+            stroke="#000000"
+            strokeWidth="1"
+          ></path>
+          <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+          <path
+            d="M15 11L12 14L9 11"
+            stroke="#000000"
+            strokeWidth="1"
+          ></path>
+        </motion.svg>
         </div>
         <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
           <motion.div style={{ x }} className="flex">
@@ -105,7 +105,7 @@ const PortfolioPage = () => {
           </motion.div>
         </div>
       </div>
-      <div className="w-full h-full flex flex-col gap-16 items-center justify-center text-center">
+      <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
         <h1 className="text-8xl">Let's collaborate!</h1>
         <div className="relative">
           <motion.svg
